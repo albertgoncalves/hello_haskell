b57dac134c5a5f77f4ec9409947d8c3705aa4f55
albertgoncalves <goncalves.albert@gmail.com>
2018-09-26 00:06:37 -0400

    Fistful of... applicatives?

---
+++ hello_haskell/fistful.hs
@@ -0,0 +1,36 @@
+{-# OPTIONS_GHC -Wall #-}
+
+import Control.Applicative
+
+-- via http://learnyouahaskell.com/a-fistful-of-monads
+
+f :: Int -> Int
+f = (* 3)
+
+f' :: Int -> Int -> Int
+f' = (*)
+
+f'' :: Int -> Maybe Int
+f'' x = Just $ f x
+
+maybeHead :: [a] -> Maybe a
+maybeHead []    = Nothing
+maybeHead (x:_) = Just x
+
+-- the Maybe instance of >>=
+applyMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b
+applyMaybe Nothing _   = Nothing
+applyMaybe (Just x) ff = ff x
+
+maxInt :: Int -> Int -> Int
+maxInt = max
+
+main :: IO ()
+main = do
+    print $ fmap f $ Just 3
+    print $ (<*>) (Just f) (Just 3)
+    print $ liftA2 f' (Just 3) (Just 3)
+    print $ f' <$> (Just 3) <*> (Just 3)
+    print $ maybeHead $ f' <$> [3] <*> [3]
+    print $ maxInt <$> Just 3 <*> Just 9
+    print $ applyMaybe (Just 3) f'' -- i.e. Just 3 >>= f''
