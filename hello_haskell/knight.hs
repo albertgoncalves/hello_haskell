85580ce09e7c2fac93ee74b4f6a20a0a45ad49ae
albertgoncalves <goncalves.albert@gmail.com>
2018-09-27 01:48:24 -0400

    Almost there...

--- hello_haskell/knight.hs
+++ hello_haskell/knight.hs
@@ -18,28 +18,28 @@ guard False = mzero
 type KnightPos = (Int, Int)
 
 moveKnight :: KnightPos -> [KnightPos]
-moveKnight (c, r) = do
-    (c', r') <- [ (c + 2, r - 1)
-                , (c + 2, r + 1)
-                , (c - 2, r - 1)
-                , (c - 2, r + 1)
-                , (c + 1, r + 2)
-                , (c + 1, r - 2)
-                , (c - 1, r + 2)
-                , (c - 1, r - 2)
-                ]
-    guard  (c' `elem` [1..8] && r' `elem` [1..8])
-    return (c', r')
+moveKnight (c, r) = filter onBoard [ (c + 2, r - 1)
+                                   , (c + 2, r + 1)
+                                   , (c - 2, r - 1)
+                                   , (c - 2, r + 1)
+                                   , (c + 1, r - 2)
+                                   , (c + 1, r + 2)
+                                   , (c - 1, r - 2)
+                                   , (c - 1, r + 2)
+                                   ]
+  where
+    onBoard (c', r') = c' `elem` [1..8] && r' `elem` [1..8]
 
-moveHistory :: KnightPos -> [[KnightPos]]
-moveHistory from = [[to, from] | to <- moveKnight from]
+moveHistory :: [KnightPos] -> [[KnightPos]]
+moveHistory []          = []
+moveHistory (from:hist) = [[to] ++ [from] ++ hist | to <- moveKnight from]
 
-demo :: IO ()
-demo = do
-    print $ moveKnight  (1, 1)
-    print $ moveKnight  (5, 5)
-    print $ moveHistory (1, 1)
-    print $ [a:b | a:b <- moveHistory (5, 5), a == (3, 4)]
+demo1 :: IO ()
+demo1 = do
+    print $ moveKnight   (1, 1)
+    print $ moveKnight   (5, 5)
+    print $ moveHistory [(1, 1)]
+    print $ [a:b | a:b <- moveHistory [(5, 5)], a == (3, 4)]
 
 in3 :: KnightPos -> [KnightPos] -- list all possible destinations afte 3 moves
 in3 start = return start >>= moveKnight >>= moveKnight >>= moveKnight
@@ -52,3 +52,8 @@ in3Do start = do
 
 canReachIn3 :: KnightPos -> KnightPos -> Bool
 canReachIn3 start end = end `elem` in3 start
+
+demo2 :: IO ()
+demo2 = do
+    let allPaths = moveHistory [(6, 2)] >>= moveHistory >>= moveHistory
+    print $ [path | path <- allPaths, head path == (6, 1)]
