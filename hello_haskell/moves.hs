d125924e735e1e7f407d03c2d68f14cabc7ca606
albertgoncalves <goncalves.albert@gmail.com>
2018-09-30 00:38:04 -0400

    Cleaner still.

--- hello_haskell/moves.hs
+++ hello_haskell/moves.hs
@@ -3,16 +3,16 @@
 moves :: (Int, Int) -> [(Int, Int)]
 moves (l, r) = combos
   where
-    xy        = [1,  2]
-    direction = [1, -1]
-    board     = [1.. 8]
+    xy        = [ 1 ,  2 ]
+    direction = [(+), (-)]
+    board     = [ 1..  8 ]
     combos    = [(ll, rr) | x  <- xy
                           , y  <- xy
                           , d  <- direction
                           , d' <- direction
                           , x /= y
-                          , let ll = (l + (x * d ))
-                          , let rr = (r + (y * d'))
+                          , let ll = d  l x
+                          , let rr = d' r y
                           , elem ll board
                           , elem rr board
                           ]
