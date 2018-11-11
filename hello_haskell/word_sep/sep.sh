4a11b7c6fda9d9298a08d3f3a5b8dd33918a4c68
albertgoncalves <goncalves.albert@gmail.com>
2018-11-10 19:31:13 -0500

    Whoops.

--- hello_haskell/word_sep/sep.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-printf '\ngp_sep.hs'
-time runhaskell gp_sep.hs < hegel.txt > hegel_gp_sep.txt
-printf '\njon_sep.hs'
-time runhaskell jon_sep.hs < hegel.txt > hegel_jon_sep.txt
-printf "\n"
-diff hegel_gp_sep.txt hegel_jon_sep.txt | head
