10bb14da316234c6608b5cdd6430560d37ccd7e5
albertgoncalves <goncalves.albert@gmail.com>
2019-01-16 22:39:08 -0500

    Hmm...

--- hello_haskell/reflect.hs
+++ hello_haskell/reflect.hs
@@ -9,8 +9,11 @@ data Message a = Message
 reflect :: Functor f => (f (a -> b) -> a) -> f (a -> b) -> f b
 reflect r f = ($ r f) <$> f
 
+formatMsg :: String -> String -> String
+formatMsg a b = "message: " ++ a ++ "\nfrom: " ++ b
+
 main :: IO ()
 main = do
-    let msg = Message "Alberto" "Hello, "
+    let msg = Message "Alberto" "Hello!"
     print msg
-    print $ messageContent $ reflect messageSender $ fmap (++) msg
+    putStrLn $ messageContent $ reflect messageSender $ fmap formatMsg msg
